206ebf1adbee1c88aa0966dcbbe3573949a51bd1
Fabian Gazzia <if20b031@example.com>
2021-12-07 22:59:12 +0100

    rest vom codebsp eingefuegt

--- src/myserver.cpp
+++ src/myserver.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+#include "include/server_class.hpp"
+
+#define PORT 6543
+#define PATH ""
+
 int main() {
-    printf("Hello Server\n");
+    std::cout << "myserver.cpp started..." << std::endl;
+    
+    Server server = Server(PORT, PATH);
+    
+    try
+    {
+        server.StartServer();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    std::cout << "myserver.cpp terminated..." << std::endl;
     return 0;
 }
--- src/server_class.cpp
+++ src/server_class.cpp
@@ -3,7 +3,7 @@
 Server::Server(int port, std::string mailSpoolDir)
 {
     _buf = 1024;
-    _port = port;
+    _port = 6543;
     _reuseVal = 1;
     _newSocket = -1;
     _createSocket = -1;
@@ -11,13 +11,19 @@ Server::Server(int port, std::string mailSpoolDir)
     _mailSpoolDir = mailSpoolDir;
 }
 
+void Server::StartServer()
+{
+    if(!InitSocket()) throw "Socket initialization.";
+    if(!InitConnection()) throw "Socket connection failed.";
+    if(!ClientConnection()) throw "Client connection failed.";
+}
+
 bool Server::InitSocket()
 {
     int reuseVal = 1;
 
     // signal handler
-    
-    if (signal(SIGINT, SignalHandler) == SIG_ERR) // expression must have class type but it has type "Server *"
+    if (signal(SIGINT, SignalHandler) == SIG_ERR)
     {
         std::cerr << "Signal can not be registered." << std::endl;
         return EXIT_FAILURE;
@@ -70,6 +76,94 @@ bool Server::InitConnection()
 
 bool Server::ClientConnection()
 {
+    while (!_abortRequested)
+    {
+        std::cout << "Waiting for connections..." << std::endl;
+
+        _addressLength = sizeof(struct sockaddr_in);
+        if ((_newSocket = accept(_createSocket, (struct sockaddr *)&_cliaddress, &_addressLength)) == -1)
+        {
+            if(_abortRequested)
+                std::cerr << "Accept error after aborted." << std::endl;
+            else
+                std::cerr << "Accept error." << std::endl;
+
+            break;
+        }
+        
+        std::cout << "Client connected from " << inet_ntoa(_cliaddress.sin_addr) << ": " << ntohs(_cliaddress.sin_port) << "..." << std::endl;
+        ClientComm(&_newSocket);
+        _newSocket = -1;
+    }
+}
+
+void *Server::ClientComm(void *data)
+{
+    int size;
+    char buffer[_buf];
+    int *currentSocket = (int *)data;
+
+    strcpy(buffer, "Welcome to myserver!\r\nPlease enter your commands...\r\n");
+    if (send(*currentSocket, buffer, strlen(buffer), 0) == -1)
+    {
+        std::cerr << "Send failed." << std::endl;
+        return NULL;
+    }
+
+    do
+    {
+        size = recv(*currentSocket, buffer, _buf - 1, 0);
+        if (size == -1)
+        {
+            if (_abortRequested)
+                std::cerr << "recv error after aborted." << std::endl;
+
+            else
+                std::cerr << "recv error." << std::endl;
+
+            break;
+        }
+
+        if (size == 0)
+        {
+            std::cout << "Client closed remote socket." << std::endl;
+            break;
+        }
+
+        // remove ugly debug message, because of the sent newline of client
+        if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n')
+        {
+            size -= 2;
+        }
+        else if (buffer[size - 1] == '\n')
+        {
+            --size;
+        }
+
+        buffer[size] = '\0';
+        printf("Message received: %s\n", buffer); // ignore error
+        std::cout << "Message received: " << buffer << std::endl;
+
+        if (send(*currentSocket, "OK", 3, 0) == -1)
+        {
+            std::cerr << "Send answer failed." << std::endl;
+            return NULL;
+        }
+    } while (strcmp(buffer, "quit") != 0 && !_abortRequested);
+}
+
+void Server::CloseSockets(int socket)
+{
+    if (socket != -1)
+    {
+        if (shutdown(socket, SHUT_RDWR) == -1)
+            std::cerr << "Shutdown socket." << std::endl;
+
+        if (close(socket) == -1)
+            std::cerr << "Close creatsockete_socket." << std::endl;
+
+        socket = -1;
+    }
 }
 
 void Server::SignalHandler(int sig)
@@ -103,18 +197,4 @@ void Server::SignalHandler(int sig)
     }
     else
         exit(sig);
-}
-
-void Server::CloseSockets(int socket)
-{
-    if (socket != -1)
-    {
-        if (shutdown(socket, SHUT_RDWR) == -1)
-            perror("shutdown socket");
-
-        if (close(socket) == -1)
-            perror("close creatsockete_socket");
-
-        socket = -1;
-    }
 }
