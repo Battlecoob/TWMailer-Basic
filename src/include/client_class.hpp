3717514359cc8d7583848b530e42cfdc605861c7
jacob-sch <jacob.schmauss@gmail.com>
2021-12-07 20:18:27 +0100

    client-structure-layout

--- src/include/client_class.hpp
+++ src/include/client_class.hpp
@@ -8,4 +8,35 @@ private:
 public:
     Client(/* args */);
     ~Client();
+
+    void readArguments(/* args */);
+    //reads arguments and wrapps them into the class structure
+
+    void createConnection(/* args */);
+    //connects to server (websocket, ports, etc...)
+
+    void clearConnection(/* args */);
+    //disconnects from server (websocket, ports, etc...)
+
+    void waitForNextCommand(/* args */);
+    //loop that waits for user input, starts other method depending on input
+
+    void SEND(/* args */);
+
+    void LIST(/* args */);
+
+    void READ(/* args */);
+
+    void DEL(/* args */);
+
+    void HELP(/* args */);
+    //lists the valid commands
+
+    //void QUIT(); ???
+
+    //void submit(/* args */); ???
+    //send message to server (maybe already done in SEND,LIST.....)
+
+
+
 };
--- src/myclient.cpp
+++ src/myclient.cpp
@@ -2,5 +2,17 @@
 
 int main() {
     printf("Hello Client\n");
+
+    // create client_class obj
+
+    //start read arguments method
+
+    //start create connection method
+
+    //start input/command method
+
+    //execute clear connection method
+
+
     return 0;
 }
